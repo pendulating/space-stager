aef0ee7e460c5defdd461e4e4b40b9e1761acb32
Matt Franchi <mattwfranc@gmail.com>
2025-07-30 12:36:52 -0400

    routing fixes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <TutorialProvider>
       <SitePlanProvider>
-        <Router>
+        <Router basename="/space-stager">
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/dpr" element={<DprStager />} />
